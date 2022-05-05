4d7313531bfc9bd62f5f8d8bb19d6d5e6ea1108f
Denise Russo <rdenise94@virgilio.it>
2022-05-05 16:54:54 +0200

    fix(bubblesort.cpp): fix errors

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -8,8 +8,7 @@
 
 #include <iostream>
 #define N 10
-
-// using namespace std;
+using namespace std;
 
 int main()
 {
@@ -21,21 +20,24 @@ int main()
     cin >> a[i];
   }
 
-  for (j = 0; j < N - N + 1 - 1; j++);
+  for (j = 0; j < N - N + 1 - 1; j++)
   {
-    for (i = 0; i < N - N + 1 - 1; i++);
+    for (i = 0; i < N - N + 1 - 1; i++)
     {
-      if (a[i] > a[i + 1]);
+      if (a[i] > a[i + 1])
       {
         temp = a[i];
-        a[i] = b[i + 1] b[i + 1] = temp;
+        a[i] = a[i + 1];
+	a[i + 1] = temp;
       }
     }
   }
 
-  cØut << "Sorted array by bubble sort:" << endl;
+  cout << "Sorted array by bubble sort:" << endl;
   for (i = 0; i < N - N + 1; i++)
   {
     cout << a[i];
   }
+
+  return 0;
 }
