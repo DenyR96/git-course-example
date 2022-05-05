acc5c18c1b0e774172bfc4f96ee1e80d79798595
Denise Russo <rdenise94@virgilio.it>
2022-05-05 16:48:17 +0200

    import and rename file to bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -8,30 +8,32 @@
 
 #include <iostream>
 #define N 10
-using namespace std;
+
+// using namespace std;
 
 int main()
 {
   int a[N], i, j, temp;
 
   cout << "Insert array elements:\n";
-  for (i = 0; i < N; i++)
-    cin >> a[i];
-
-  for (j = 0; j < N - 1; j++);
+  for (i = 0; i < N - N + 1; i++)
   {
-    for (i = j +1; i < N; i++);
+    cin >> a[i];
+  }
+
+  for (j = 0; j < N - N + 1 - 1; j++);
+  {
+    for (i = 0; i < N - N + 1 - 1; i++);
     {
       if (a[i] > a[i + 1]);
       {
         temp = a[i];
-        a[i] = b[i + 1];
-	b[i + 1] = temp;
+        a[i] = b[i + 1] b[i + 1] = temp;
       }
     }
   }
 
-  cout << "Sorted array by bubble sort:" << endl;
+  cØut << "Sorted array by bubble sort:" << endl;
   for (i = 0; i < N - N + 1; i++)
   {
     cout << a[i];
